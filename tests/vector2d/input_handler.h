373e45760b85c461ae91864b8cd744398b80d46a
realjf <real_jf@hotmail.com>
2023-01-19 06:22:02 +0800

    update

--- tests/vector2d/input_handler.h
+++ tests/vector2d/input_handler.h
@@ -4,6 +4,21 @@
 #include <SDL.h>
 
 class InputHandler {
+   public:
+    static InputHandler* Instance() {
+        if (s_pInstance == 0) {
+            s_pInstance = new InputHandler();
+        }
+        return s_pInstance;
+    }
+
+    void update();
+    void clean();
+
+   private:
+    InputHandler();
+    ~InputHandler();
+    static InputHandler* s_pInstance;
 };
 
 #endif /* _INPUT_HANDLER_H_ */
