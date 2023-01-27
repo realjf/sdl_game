8d55b740429b5ddf62babe6f4f6d1a94930f4f77
realjf <real_jf@hotmail.com>
2023-01-28 07:29:49 +0800

    update

--- tests/vector2d/input_handler.cpp
+++ tests/vector2d/input_handler.cpp
@@ -1,1 +1,11 @@
 #include "input_handler.h"
+
+void InputHandler::update()
+{
+
+}
+
+void InputHandler::clean()
+{
+    
+}
--- tests/vector2d/input_handler.h
+++ tests/vector2d/input_handler.h
@@ -2,6 +2,7 @@
 #define _INPUT_HANDLER_H_
 
 #include <SDL.h>
+#include <vector>
 
 class InputHandler {
    public:
@@ -15,10 +16,18 @@ class InputHandler {
     void update();
     void clean();
 
+    void initialiseJoysticks();
+    bool joysticksInitialised() {
+        return m_bJoysticksInitialised;
+    }
+
    private:
     InputHandler();
     ~InputHandler();
     static InputHandler* s_pInstance;
+
+    std::vector<SDL_Joystick*> m_joysticks;
+    bool m_bJoysticksInitialised;
 };
 
 #endif /* _INPUT_HANDLER_H_ */
