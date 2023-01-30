a187e7223fb914f1e05ca8ec147b0a44ca7fc5cd
realjf <real_jf@hotmail.com>
2023-01-30 12:32:09 +0800

    update

--- tests/menu_state/player.cpp
+++ tests/menu_state/player.cpp
@@ -31,8 +31,10 @@ void Player::update() {
 
     handleInput();
 
-    m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
-    // m_currentRow = int(((SDL_GetTicks() / 100) % 4));
+    // only for helicopter
+    m_currentFrame = 0;
+    // m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
+    m_currentRow = int(((SDL_GetTicks() / 10) % 3));
     std::cout << "frame: " << m_currentFrame << std::endl;
     // m_acceleration.setX(1);
     SDLGameObject::update();
