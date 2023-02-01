bc7edd5db3d418558ab30289ab77044ae9c1043d
realjf <real_jf@hotmail.com>
2023-02-02 06:03:12 +0800

    update

---
+++ tests/include/platform/platforms.h
@@ -0,0 +1,24 @@
+#ifndef _PLATFORMS_H_
+#define _PLATFORMS_H_
+
+#if defined(_WIN32) || defined(_WIN64)
+
+#define PLATFORM_WINDOWS 1
+#define PLATFORM_ANDROID 0
+#define PLATFORM_IOS 0
+
+#elif defined(__ANDROID__)
+
+#define PLATFORM_WINDOWS 0
+#define PLATFORM_ANDROID 1
+#define PLATFORM_IOS 0
+
+#elif defined(TARGET_OS_IPHONE)
+
+#define PLATFORM_WINDOWS 0
+#define PLATFORM_ANDROID 0
+#define PLATFORM_IOS 1
+
+#endif
+
+#endif /* _PLATFORMS_H_ */
